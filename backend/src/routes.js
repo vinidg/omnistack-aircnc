e40661ce2f77d25314ae356c651bedee8e326602
Vinicius Duarte Galdino <vduarteg@everis.com>
2019-10-01 16:21:48 -0300

    backend em node

---
+++ backend/src/routes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
+const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
+
+const routes = express.Router()
+const upload = multer(uploadConfig)
+
+routes.get('/', (req, res) => {
+    return res.json({ message: "Listening on port 3333" })
+ })
+
+ routes.post('/users', SessionController.store)
+ 
+ routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+ routes.get('/spots', SpotController.index)
+
+ routes.get('/dashboard', DashboardController.show)
+
+ routes.post('/spots/:spot_id/booking', BookingController.store)
+
+ module.exports = routes;
