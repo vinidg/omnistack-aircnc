5a15e8136e0e5c0b96c6051e71276bbe2ef0ea90
vinidg <vinidgdon@gmail.com>
2019-10-06 17:52:35 -0300

    mobile com socket io

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react'
+import {
+    SafeAreaView,
+    StyleSheet,
+    Image,
+    AsyncStorage,
+    ScrollView,
+    TouchableOpacity,
+    Alert
+    
+} from 'react-native'
+import socketio from 'socket.io-client'
+
+import SpotList from '../components/SpotList'
+
+import logo from '../assets/logo.png'
+
+export default function List({ navigation }) {
+    const [techs, setTechs] = useState([])
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.1.166:3333', {
+                query: { user_id }
+            })
+
+            socket.on('booking_response', booking => [
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+            ])
+        })
+    }, [])
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs')
+            .then(storageTechs => {
+                const techsArray = storageTechs.split(',').map(tech => tech.trim())
+                setTechs(techsArray)
+            })
+    }, [])
+
+    async function handleLogoff(){
+        await AsyncStorage.removeItem('user')
+        navigation.navigate('Login')
+        console.log("Foi");
+        
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity onPress={handleLogoff}>
+                <Image style={styles.logo} source={logo} />
+            </TouchableOpacity>
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 10
+    }
+})
