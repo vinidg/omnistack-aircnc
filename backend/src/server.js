c53eb0d0638f9fd8146a432d35230518f7b838c1
vinidg <vinidgdon@gmail.com>
2019-10-06 00:20:04 -0300

    routa para capturar imagem no backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -9,7 +11,9 @@ mongoose.connect('mongodb+srv://vinidg-dev:pXs3S3ZygIEvMSI3@cluster0-mheor.mongo
     useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..','uploads')))
 app.use(routes)
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+
 function App() {
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
       <div className="content">
-        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa</p>
-      <form>
-        <label htmlFor="email">E-mail *</label>
-        <input 
-        type="email" 
-        id="email" 
-        placeholder="Seu melhor e-mail" />
-
-        <button className="btn" type="submit">Entrar</button>
-      </form>
+        <Routes />
+        
       </div>
     </div>
   );
